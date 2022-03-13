7458a6b72d16c183a79b547fb636e61f540095e4
Ivan Budoš <ibudos@etfos.hr>
2022-03-13 22:46:40 +0100

    added simple selenium test (task 2)

---
+++ LearningTestAutomation/LearningTestAutomation/Class1.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningTestAutomation
+{
+    class Class1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+            Console.WriteLine("Setup done");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.youtube.com/";
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            
+            IWebElement agreeButton = wait.Until(e => e.FindElement(By.XPath("//*[contains(text(), 'Slažem se')]")));
+            agreeButton.Click();
+            Console.WriteLine("agreeButton - pressed");
+
+            IWebElement searchQuery = wait.Until(e => e.FindElement(By.Name("search_query")));
+            Console.WriteLine("searchQuery - found");
+
+            IWebElement searchButton = driver.FindElement(By.Id("search-icon-legacy"));
+            Console.WriteLine("searchButton - found");
+
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);  // not working for some reason
+            System.Threading.Thread.Sleep(1000); // yikes! 
+
+            //element not interactable exception - without pause/sleep
+            searchQuery.SendKeys("Won't Rick Roll you, I promise");
+            Console.WriteLine("Won't Rick Roll you - text sent");
+            System.Threading.Thread.Sleep(3000);
+
+            IWebElement searchClearButton = wait.Until(e => e.FindElement(By.Id("search-clear-button")));
+            Console.WriteLine("searchClearButton - found");
+            
+
+            searchClearButton.Click();
+            Console.WriteLine("searchClearButton - pressed");
+
+            searchQuery.SendKeys("Rick roll");
+            searchButton.Click();
+
+            //TODO: learn how to select an item from search results
+            //driver.FindElement(By.XPath("/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]")).Click();
+            System.Threading.Thread.Sleep(2000);
+            searchClearButton.Click();            
+            searchQuery.SendKeys("'cause I don't know how xD");
+
+            driver.Manage().Window.Maximize();
+
+            Console.WriteLine("Test done");
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            //driver.Close();
+            Console.WriteLine("TearDown done");
+        }
+    }
+}
